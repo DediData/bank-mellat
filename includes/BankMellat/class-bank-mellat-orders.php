d19c9cd98587bca3d51bb8077d62d9169322d7d1
DediData <33499600+Dedi-Data@users.noreply.github.com>
2024-03-24 20:26:53 +0330

    standards

--- includes/BankMellat/class-bank-mellat-orders.php
+++ includes/BankMellat/class-bank-mellat-orders.php
@@ -66,6 +66,7 @@ final class Bank_Mellat_Orders extends \DediData\Singleton {
 	 * Orders page
 	 * 
 	 * @return void
+	 * @SuppressWarnings(PHPMD.Superglobals)
 	 */
 	public function orders() {
 		?>
@@ -82,7 +83,6 @@ final class Bank_Mellat_Orders extends \DediData\Singleton {
 			}
 			?>
 			</h2>
-			
 			<div class="stat-container">
 				<!-- /stat-holder -->
 				<div class="stat-holder">
@@ -92,7 +92,6 @@ final class Bank_Mellat_Orders extends \DediData\Singleton {
 					</div>
 				<!-- /stat -->
 				</div>
-				
 				<!-- /stat-holder -->
 				<div class="stat-holder">
 					<div class="stat">							
@@ -101,7 +100,6 @@ final class Bank_Mellat_Orders extends \DediData\Singleton {
 					</div>
 				<!-- /stat -->
 				</div>
-				
 				<!-- /stat-holder -->
 				<div class="stat-holder">
 					<div class="stat">							
@@ -111,157 +109,157 @@ final class Bank_Mellat_Orders extends \DediData\Singleton {
 				<!-- /stat -->
 				</div>
 			</div>
-
-	   
 			<div id="post-stuff">
 				<div id="post-body" class="meta-box-holder columns-2">
-						<div id="post-body-content">
-						<?php
-						$get_order_id = filter_input( \INPUT_GET, 'orderId', \FILTER_SANITIZE_FULL_SPECIAL_CHARS );
-						$get_page     = filter_input( \INPUT_GET, 'page', \FILTER_SANITIZE_FULL_SPECIAL_CHARS );
-						if ( null === $get_order_id ) {
-							?>
-							<div class="meta-box-sortables ui-sortable">
-								<form id="persons-table" method="get">
-								<input type="hidden" name="page" value="<?php echo esc_attr( $get_page ); ?>"/>
-								
-								<?php
-									$table = new Orders_list();
-									$table->prepare_items();
-									$table->display();
-								?>
-								</form>
-							</div>
+					<div id="post-body-content">
+					<?php
+					$get_order_id = filter_input( \INPUT_GET, 'orderId', \FILTER_SANITIZE_FULL_SPECIAL_CHARS );
+					$get_page     = filter_input( \INPUT_GET, 'page', \FILTER_SANITIZE_FULL_SPECIAL_CHARS );
+					if ( null === $get_order_id ) {
+						?>
+						<div class="meta-box-sortables ui-sortable">
+							<form id="persons-table" method="get">
+							<input type="hidden" name="page" value="<?php echo esc_attr( $get_page ); ?>"/>
 							<?php
-						}
-						if ( null !== $get_order_id ) {
+								$table = new Orders_list();
+								$table->prepare_items();
+								$table->display();
 							?>
-								<div style="display: block;" id="order-<?php esc_attr( $get_order_id ); ?>" class="postbox">
-									<h2 class="handle ui-sortable-handle"><span><?php /* Translators: replace %s with order id */  printf( esc_html__( 'Transaction details %s', 'bank-mellat' ), esc_html( $get_order_id ) ); ?></span></h2>
-									<div class="inside">
-										<?php
-										$wpdb       = $GLOBALS['wpdb'];
-										$table_name = $wpdb->prefix . 'WPBEGPAY_orders';
-										$query      = $wpdb->prepare( "SELECT * FROM $table_name WHERE order_id = %d", $get_order_id );
-										$get_order  = $wpdb->get_results( $query );
-										if ( $get_order ) {
-											
-											foreach ( $get_order as $order ) {
-												if ( $order->order_settle == 'yes' ) {
-													$settel = "انجام شده است";
-												} else {
-													$settel = "انجام نشده است";
-												}
-												if ( $order->order_status == 'yes' ) {
-													$status = "انجام شده است";
-												} else {
-													$status = "انجام نشده است";
-												}
-												echo ( '
-														<style>
-															table, th, td {
-																border: 1px solid rgb(206, 199, 199);
-															} 
-														</style>
-														<table style="width: 100%; border: 1px solid rgb(206, 199, 199);" dir="rtl" border="1" cellpadding="10">
-															<tbody>
-																<tr>
-																<td>#</td>
-																<td>' . $order->order_id . '</td>
-																</tr>
-																<tr>
-																<td>نام و نام خانوادگي</td>
-																<td>' . $order->order_name_surname . '</td>
-																</tr>
-																<tr>
-																<td>آدرس ايميل</td>
-																<td>' . $order->order_email . '</td>
-																</tr>
-																<tr>
-																<td>شماره تلفن</td>
-																<td>' . $order->order_phone . '</td>
-																</tr>
-																<tr>
-																<td>توضيحات</td>
-																<td>' . $order->order_des . '</td>
-																</tr>
-																<tr>
-																<td>وضعيت پرداخت</td>
-																<td>' . $status . '</td>
-																</tr>
-																<tr>
-																<td>ستل</td>
-																<td>' . $settel . '</td>
-																</tr>
-																<tr>
-																<td>تاريخ</td>
-																<td>' . $order->order_date . '</td>
-																</tr>
-																<tr>
-																<td>آي پي</td>
-																<td>' . $order->order_ip . '</td>
-																</tr>
-																<tr>
-																<td>مبلغ(ريال)</td>
-																<td>' . $order->order_amount . '</td>
-																</tr>
-																<tr>
-																<td>رسيد ديجيتالي سفارش</td>
-																<td>' . $order->order_referenceId . '</td>
-																</tr>
-																<tr>
-																<td>شماره سفارش</td>
-																<td>' . $order->order_orderid . '</td>
-																</tr>
-																<tr>
-																<td>شماره تراکنش</td>
-																<td>' . $order->order_refid . '</td>
-																</tr>
-															</tbody>
-														</table>' );
-											}
-										}
-										?>
-									</div>
-								</div>	
-								
-								<?php printf( '<a class="button" href="%s">%s</a>', admin_url( 'admin.php?page=bank-mellat', 'http' ), 'بازگشت' ); ?>
-								
-							<?php } ?>
+							</form>
 						</div>
+							<?php
+					}
+					if ( null !== $get_order_id ) {
+						?>
+						<div style="display: block;" id="order-<?php esc_attr( $get_order_id ); ?>" class="postbox">
+							<h2 class="handle ui-sortable-handle"><span><?php /* Translators: replace %s with order id */  printf( esc_html__( 'Transaction details %s', 'bank-mellat' ), esc_html( $get_order_id ) ); ?></span></h2>
+							<div class="inside">
+							<?php
+							// phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable.DisallowedSuperGlobalVariable
+							$wpdb       = $GLOBALS['wpdb'];
+							$table_name = $wpdb->prefix . 'WPBEGPAY_orders';
+							$cache_key  = 'bank_mellat_get_order_' . $get_order_id;
+							$get_order  = wp_cache_get( $cache_key, 'bank_mellat_orders' );
+							// If the data is not cached, fetch it from the database and cache it
+							if ( false === $get_order ) {
+								$get_order = $wpdb->get_results( $wpdb->prepare( 'SELECT * FROM %s WHERE order_id = %d', $table_name, $get_order_id ) );
+								// Cache the result for future use
+								wp_cache_set( $cache_key, $get_order, 'bank_mellat_orders' );
+							}
+							if ( $get_order ) {
+								foreach ( $get_order as $order ) {
+									$settle = 'yes' === $order->order_settle ? esc_html__( 'It has been done', 'bank-mellat' ) : esc_html__( 'It has not been done', 'bank-mellat' );
+									$status = 'yes' === $order->order_status ? esc_html__( 'It has been done', 'bank-mellat' ) : esc_html__( 'It has not been done', 'bank-mellat' );
+									echo '
+										<style>
+										table, th, td {
+											border: 1px solid rgb(206, 199, 199);
+										} 
+										</style>
+										<table style="width: 100%; border: 1px solid rgb(206, 199, 199);" dir="rtl" border="1" cellpadding="10">
+											<tbody>
+												<tr>
+													<td>#</td>
+													<td>' . esc_html( $order->order_id ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'First and last name', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $order->order_name_surname ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'Email Address', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $order->order_email ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'Phone Number', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $order->order_phone ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'Description', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $order->order_des ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'Payment Status', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $status ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'Settle', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $settle ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'Date', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $order->order_date ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'IP', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $order->order_ip ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'Order Amount', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $order->order_amount ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'Order Reference ID', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $order->order_referenceId ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'Order ID', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $order->order_orderid ) . '</td>
+												</tr>
+												<tr>
+													<td>' . esc_html__( 'Transaction Number', 'bank-mellat' ) . '</td>
+													<td>' . esc_html( $order->order_refid ) . '</td>
+												</tr>
+											</tbody>
+										</table>
+									';
+								}//end foreach
+							}//end if
+							?>
+							</div>
+						</div>
+						<?php
+						printf( '<a class="button" href="%s">%s</a>', esc_url( admin_url( 'admin.php?page=bank-mellat', 'admin' ) ), esc_html__( 'Return', 'bank-mellat' ) );
+					}//end if
+					?>
+					</div>
 				</div>
-				
-				<div class="sidebar-container"><a href="http://www.wp-beginner.ir/product/%d8%a7%d9%81%d8%b2%d9%88%d9%86%d9%87-%d8%af%d8%b1%da%af%d8%a7%d9%87-%d9%be%d8%b1%d8%af%d8%a7%d8%ae%d8%aa-%d8%a8%d8%a7%d9%86%da%a9-%d9%87%d8%a7%db%8c-%d8%a7%db%8c%d8%b1%d8%a7%d9%86%db%8c/" target="_blank"><img src="<?php echo plugins_url( 'bank-mellat/images/banner.jpg' ); ?>" /></a></div>
-			
 			</div>
-
 		</div>
-
 		<?php
 	}
 
 	/**
 	 * Forms page
 	 *
+	 * @param string $arg Tag.
 	 * @return void
+	 * @SuppressWarnings(PHPMD.Superglobals)
 	 */
-	private function orders_widget( $arg ) {
-		 
-		global $wpdb;
+	private function orders_widget( string $arg ) {
+		// phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable.DisallowedSuperGlobalVariable
+		$wpdb         = $GLOBALS['wpdb'];
 		$orders_table = $this->orders_table_name;
 		switch ( $arg ) {
 			case 'successful_pay_money':
-				$q = $wpdb->get_var( "select SUM(order_amount) AS priceCount from $orders_table where order_status='yes'" );
-			break;
-
+				$cache_key = 'sum_order_amount_yes';
+				$query     = wp_cache_get( $cache_key, 'bank_mellat_orders' );
+				if ( false === $query ) {
+					$query = $wpdb->get_var( $wpdb->prepare( "SELECT SUM(order_amount) AS priceCount FROM %s WHERE order_status = 'yes'", $orders_table ) );
+					// Cache the result for future use
+					wp_cache_set( $cache_key, $query, 'bank_mellat_orders' );
+				}
+				break;
 			case 'successful_pay':
-				$q = $wpdb->get_var( "select count(*) from $orders_table where order_status='yes'" );
-			break;
-
+				$query = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM %s WHERE order_status = 'yes'", $orders_table ) );
+				break;
 			case 'unsuccessful_pay':
-				$q = $wpdb->get_var( "select count(*) from $orders_table where order_status='no'" );
-			break;
+				$query = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM %s WHERE order_status = 'no'", $orders_table ) );
+				break;
+			default:
+				// nothing for default
+				break;
 		}
-		echo number_format( $q );
+		echo number_format( $query );
 	}
 }
