38af10c2b5e617c9745f61ba92e9fa6b7743a8aa
DediData <33499600+Dedi-Data@users.noreply.github.com>
2024-03-27 21:00:37 +0330

    standards

--- includes/BankMellat/class-bank-mellat-sms.php
+++ includes/BankMellat/class-bank-mellat-sms.php
@@ -35,23 +35,24 @@ final class Bank_Mellat_Sms extends \DediData\Singleton {
 		<div style="display:none">
 			<?php
 			if ( 'diakosms' === $sms_service ) {
-				$f = wp_remote_get( 'http://www.diakosms.ir/WsSms.asmx/sendsms?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				$result = wp_remote_get( 'http://www.diakosms.ir/WsSms.asmx/sendsms?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
 			} elseif ( 'f2usms' === $sms_service ) {
-				$f = wp_remote_get( 'http://sms.f2u.ir/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				$result = wp_remote_get( 'http://sms.f2u.ir/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
 			} elseif ( 'mediana' === $sms_service ) {
-				$f = wp_remote_get( 'http://185.4.28.180/class/sms/webservice/send_url.php?from=' . $sms_line_number . '&to=' . $admin_mobile . '&msg=' . urlencode( $sms_text ) . '&uname=' . $sms_user_name . '&pass=' . $sms_password );
+				$result = wp_remote_get( 'http://185.4.28.180/class/sms/webservice/send_url.php?from=' . $sms_line_number . '&to=' . $admin_mobile . '&msg=' . urlencode( $sms_text ) . '&uname=' . $sms_user_name . '&pass=' . $sms_password );
 				if ( '' !== $customer_mob ) {
-					$ff = wp_remote_get( 'http://185.4.28.180/class/sms/webservice/send_url.php?from=' . $sms_line_number . '&to=' . $customer_mob . '&msg=' . urlencode( $sms_text ) . '&uname=' . $sms_user_name . '&pass=' . $sms_password );
+					$result_customer = wp_remote_get( 'http://185.4.28.180/class/sms/webservice/send_url.php?from=' . $sms_line_number . '&to=' . $customer_mob . '&msg=' . urlencode( $sms_text ) . '&uname=' . $sms_user_name . '&pass=' . $sms_password );
 				}
 			} elseif ( 'aminsms' === $sms_service ) {
-				$f = wp_remote_get( 'http://37.130.202.188/class/sms/webservice/send_url.php?from=' . $sms_line_number . '&to=' . $admin_mobile . '&msg=' . urlencode( $sms_text ) . '&uname=' . $sms_user_name . '&pass=' . $sms_password );
+				$result = wp_remote_get( 'http://37.130.202.188/class/sms/webservice/send_url.php?from=' . $sms_line_number . '&to=' . $admin_mobile . '&msg=' . urlencode( $sms_text ) . '&uname=' . $sms_user_name . '&pass=' . $sms_password );
 				if ( '' !== $customer_mob ) {
-					$ff = wp_remote_get( 'http://37.130.202.188/class/sms/webservice/send_url.php?from=' . $sms_line_number . '&to=' . $customer_mob . '&msg=' . urlencode( $sms_text ) . '&uname=' . $sms_user_name . '&pass=' . $sms_password );
+					$result_customer = wp_remote_get( 'http://37.130.202.188/class/sms/webservice/send_url.php?from=' . $sms_line_number . '&to=' . $customer_mob . '&msg=' . urlencode( $sms_text ) . '&uname=' . $sms_user_name . '&pass=' . $sms_password );
 				}
 			} elseif ( 'iransms' === $sms_service ) {
-				$f = wp_remote_get( 'http://panel.iransms.cc/url.php?from=' . $sms_line_number . '&to=' . $admin_mobile . '&text=' . urlencode( $sms_text ) . '&username=' . $sms_user_name . '&password=' . $sms_password );
+				$result = wp_remote_get( 'http://panel.iransms.cc/url.php?from=' . $sms_line_number . '&to=' . $admin_mobile . '&text=' . urlencode( $sms_text ) . '&username=' . $sms_user_name . '&password=' . $sms_password );
 				if ( '' !== $customer_mob ) {
-					$ff = wp_remote_get( 'http://panel.iransms.cc/url.php?from=' . $sms_line_number . '&to=' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&username=' . $sms_user_name . '&password=' . $sms_password );
+					$result_customer = wp_remote_get( 'http://panel.iransms.cc/url.php?from=' . $sms_line_number . '&to=' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&username=' . $sms_user_name . '&password=' . $sms_password );
+					echo esc_html( $result_customer );
 				}
 			} elseif ( 'melipayamak' === $sms_service ) {
 				// turn off the WSDL cache
@@ -113,20 +114,20 @@ final class Bank_Mellat_Sms extends \DediData\Singleton {
 					$parameters['udh']      = '';
 					$parameters['recId']    = array( 0 );
 					$parameters['status']   = 0x0;
-					echo $client->SendSms( $parameters )->SendSmsResult;
+					echo esc_html( $client->SendSms( $parameters )->SendSmsResult );
 				} catch ( SoapFault $ex ) {
 					// empty
 				}
 			} elseif ( 'f2usms2' === $sms_service ) {
-				$f = file_get_contents( 'http://sms.panel2u.ir/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				$result = wp_remote_get( 'http://sms.panel2u.ir/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
 			} elseif ( 'fpayamak' === $sms_service ) {
-				$f = file_get_contents( 'http://login.payamakde.com/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				$result = wp_remote_get( 'http://login.payamakde.com/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
 			} elseif ( 'freersms' === $sms_service ) {
 				if ( ! class_exists( 'nusoap_base' ) ) {
 					require_once $this->plugin_folder . '/includes/Composer/autoload.php';
 				}
 				$client = new nusoap_client( 'http://sms.freer.ir/gateway/index.php?wsdl', 'wsdl' );
-				$res    = $client->call(
+				$result = $client->call(
 					'SendSMS',
 					array(
 						$sms_user_name,
@@ -138,7 +139,7 @@ final class Bank_Mellat_Sms extends \DediData\Singleton {
 				);
 			} elseif ( 'hezarnevis' === $sms_service ) {
 				$url    = 'http://panel.hezarnevis.com/API/SendSms.ashx?username=' . $sms_user_name . '&password=' . $sms_password . '&from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text );
-				$result = file_get_contents( $url );
+				$result = wp_remote_get( $url );
 			} elseif ( 'idehsms' === $sms_service ) {
 				$url                      = 'http://sms.idehsms.ir/remote.php';
 				$parameters['Number']     = "$sms_line_number";
@@ -152,7 +153,7 @@ final class Bank_Mellat_Sms extends \DediData\Singleton {
 				curl_setopt( $ch, \CURLOPT_TIMEOUT, 310000 );
 				curl_setopt( $ch, \CURLOPT_RETURNTRANSFER, 1 );
 				curl_setopt( $ch, \CURLOPT_POSTFIELDS, $parameters );
-				$data1 = curl_exec( $ch );
+				$result = curl_exec( $ch );
 				curl_close( $ch );
 			} elseif ( 'idehsms3000' === $sms_service ) {
 				if ( ! class_exists( 'nusoap_base' ) ) {
@@ -166,7 +167,7 @@ final class Bank_Mellat_Sms extends \DediData\Singleton {
 				$soap_client                   = new nusoap_client( 'http://ws.idehsms.ir/index.php?wsdl', 'wsdl' );
 				$soap_client->soap_defencoding = 'UTF-8';
 				$soap_proxy                    = $soap_client->getProxy();
-				$res                           = $soap_proxy->SendSMS( $username, $password, $reciever, $text, $sender );
+				$result                        = $soap_proxy->SendSMS( $username, $password, $reciever, $text, $sender );
 			} elseif ( 'irpayamak' === $sms_service ) {
 				$username = "$sms_user_name";
 				$password = "$sms_password";
@@ -197,38 +198,38 @@ final class Bank_Mellat_Sms extends \DediData\Singleton {
 				$result = curl_exec( $ch );
 				curl_close( $ch );
 			} elseif ( 'panizsms' === $sms_service ) {
-				$f = file_get_contents( 'http://panel.panizsms.com/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				$result = wp_remote_get( 'http://panel.panizsms.com/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
 			} elseif ( 'parandsms' === $sms_service ) {
-				$f = file_get_contents( 'http://parandsms.ir/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				$result = wp_remote_get( 'http://parandsms.ir/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
 			} elseif ( 'persiansms' === $sms_service ) {
 				$my_class = new SoapClient(
 					'http://www.persiansms.info/webservice/smsService.php?wsdl',
-					array(
-						'trace' => 1,
-					)
+					array( 'trace' => 1 )
 				);
-				$smsid    = $my_class->send_sms( $sms_user_name, $sms_password, $sms_line_number, $admin_mobile, $sms_text );
+				$result   = $my_class->send_sms( $sms_user_name, $sms_password, $sms_line_number, $admin_mobile, $sms_text );
 				if ( '' !== $customer_mob ) {
-					$smsids = $my_class->send_sms( $sms_user_name, $sms_password, $sms_line_number, $customer_mob, $sms_text );
+					$result = $my_class->send_sms( $sms_user_name, $sms_password, $sms_line_number, $customer_mob, $sms_text );
 				}
 			} elseif ( 'mcisms' === $sms_service ) {
-				$f = file_get_contents( 'http://www.p.mcisms.net/send_via_get/send_sms.php?username=' . $sms_user_name . '&password=' . $sms_password . '&sender_number=' . $sms_line_number . '&receiver_number=' . $admin_mobile . '&note=' . urlencode( $sms_text ) );
+				$result = wp_remote_get( 'http://www.p.mcisms.net/send_via_get/send_sms.php?username=' . $sms_user_name . '&password=' . $sms_password . '&sender_number=' . $sms_line_number . '&receiver_number=' . $admin_mobile . '&note=' . urlencode( $sms_text ) );
 				if ( '' !== $customer_mob ) {
-					echo $f = file_get_contents( 'http://www.p.mcisms.net/send_via_get/send_sms.php?username=' . $sms_user_name . '&password=' . $sms_password . '&sender_number=' . $sms_line_number . '&receiver_number=' . $customer_mob . '&note=' . urlencode( $sms_text ) );
+					$result = wp_remote_get( 'http://www.p.mcisms.net/send_via_get/send_sms.php?username=' . $sms_user_name . '&password=' . $sms_password . '&sender_number=' . $sms_line_number . '&receiver_number=' . $customer_mob . '&note=' . urlencode( $sms_text ) );
+					echo esc_html( $result );
 				}
 			} elseif ( 'textsms' === $sms_service ) {
-				$f = file_get_contents( 'http://textsms.ir/send_via_get/send_sms.php?username=' . $sms_user_name . '&password=' . $sms_password . '&sender_number=' . $sms_line_number . '&receiver_number=' . $admin_mobile . ',' . $customer_mob . '&note=' . urlencode( $sms_text ) );
+				$result = wp_remote_get( 'http://textsms.ir/send_via_get/send_sms.php?username=' . $sms_user_name . '&password=' . $sms_password . '&sender_number=' . $sms_line_number . '&receiver_number=' . $admin_mobile . ',' . $customer_mob . '&note=' . urlencode( $sms_text ) );
 			} elseif ( 'payamresan' === $sms_service ) {
-				$f = file_get_contents( 'http://www.payam-resan.com/APISend.aspx?Username=' . $sms_user_name . '&password=' . $sms_password . '&from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) );
+				$result = wp_remote_get( 'http://www.payam-resan.com/APISend.aspx?Username=' . $sms_user_name . '&password=' . $sms_password . '&from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) );
 			} elseif ( 'samanpayamak' === $sms_service ) {
+				$get             = array();
 				$get['username'] = $sms_user_name;
 				$get['password'] = $sms_password;
 				$get['from']     = $sms_line_number;
 				$get['To']       = $admin_mobile . ',' . $customer_mob;
 				$get['text']     = $sms_text;
-				$baseURL         = 'http://samanpayamak.ir/API/SendSms.ashx';
-				$filename        = $baseURL . '?' . http_build_query( $get );
-				$res             = file_get_contents( $filename );
+				$base_url        = 'http://samanpayamak.ir/API/SendSms.ashx';
+				$filename        = $base_url . '?' . http_build_query( $get );
+				$result          = wp_remote_get( $filename );
 			} elseif ( 'shabnam1' === $sms_service ) {
 				$url = 'http://37.130.202.188/services.jspd';
 
@@ -238,21 +239,22 @@ final class Bank_Mellat_Sms extends \DediData\Singleton {
 					'pass'    => $sms_password,
 					'from'    => $sms_line_number,
 					'message' => $sms_text,
-					'to'      => json_encode( $rcpt_nm ),
+					'to'      => wp_json_encode( $rcpt_nm ),
 					'op'      => 'send',
 				);
 
 				$handler = curl_init( $url );
-				curl_setopt( $handler, CURLOPT_CUSTOMREQUEST, 'POST' );
-				curl_setopt( $handler, CURLOPT_POSTFIELDS, $param );
-				curl_setopt( $handler, CURLOPT_RETURNTRANSFER, true );
-				$response2 = curl_exec( $handler );
+				curl_setopt( $handler, \CURLOPT_CUSTOMREQUEST, 'POST' );
+				curl_setopt( $handler, \CURLOPT_POSTFIELDS, $param );
+				curl_setopt( $handler, \CURLOPT_RETURNTRANSFER, true );
+				$result = curl_exec( $handler );
+				echo esc_html( $result );
 			} elseif ( 'sgmsms' === $sms_service ) {
-				$f = file_get_contents( 'http://panel.sigmasms.ir/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				$result = wp_remote_get( 'http://panel.sigmasms.ir/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
 			} elseif ( 'banehsms' === $sms_service ) {
-				$f = file_get_contents( 'http://banehsms.ir/post/SendWithDelivery.ashx?from' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				$result = wp_remote_get( 'http://banehsms.ir/post/SendWithDelivery.ashx?from' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
 			} elseif ( 'shabnam' === $sms_service ) {
-				$f = file_get_contents( 'http://shabnam-sms.ir/API/SendSms.ashx?username=' . $sms_user_name . '&password=' . $sms_password . '&from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) );
+				$result = wp_remote_get( 'http://shabnam-sms.ir/API/SendSms.ashx?username=' . $sms_user_name . '&password=' . $sms_password . '&from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) );
 			} elseif ( 'smsclick' === $sms_service ) {
 				try {
 					$client                 = new SoapClient( 'http://sms.dorbid.ir/post/send.asmx?wsdl', array( 'encoding' => 'UTF-8' ) );
@@ -265,21 +267,22 @@ final class Bank_Mellat_Sms extends \DediData\Singleton {
 					$parameters['udh']      = '';
 					$parameters['recId']    = array( 0 );
 					$parameters['status']   = 0x0;
-					echo $client->GetCredit(
+					$get_credit             = $client->GetCredit(
 						array(
 							'username' => 'wsdemo',
 							'password' => 'wsdemo',
 						)
-					)->GetCreditResult;
-					echo $client->SendSms( $parameters )->SendSmsResult;
-					echo $status;
+					);
+					echo esc_html( $get_credit->GetCreditResult );
+					echo esc_html( $client->SendSms( $parameters )->SendSmsResult );
 				} catch ( SoapFault $ex ) {
-					echo $ex->faultstring;
-				}
+					echo esc_html( $ex->faultstring );
+				}//end try
 			} elseif ( 'spadsms' === $sms_service ) {
-				$f = file_get_contents( 'http://spadsms.net/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				$result = wp_remote_get( 'http://spadsms.net/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
 			} elseif ( 'wstdsms' === $sms_service ) {
-				$f = file_get_contents( 'http://sms.webstudio.ir/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				$result = wp_remote_get( 'http://sms.webstudio.ir/post/sendSMS.ashx?from=' . $sms_line_number . '&to=' . $admin_mobile . ',' . $customer_mob . '&text=' . urlencode( $sms_text ) . '&password=' . $sms_password . '&username=' . $sms_user_name );
+				echo esc_html( $result );
 			} elseif ( 'hostiran' === $sms_service ) {
 				$options = array(
 					'login'    => $sms_user_name,
@@ -287,25 +290,26 @@ final class Bank_Mellat_Sms extends \DediData\Singleton {
 				);
 				$client  = new SoapClient( 'http://sms.hostiran.net/webservice/?WSDL', $options );
 				try {
-					$messageId = $client->send( $admin_mobile, $sms_text );
+					$message_id = $client->send( $admin_mobile, $sms_text );
 					sleep( 3 );
-					print( $client->deliveryStatus( $messageId ) );
-					var_dump( $client->accountInfo() );
+					echo esc_html( $client->deliveryStatus( $message_id ) );
+					echo esc_html( $client->accountInfo() );
 				} catch ( SoapFault $sf ) {
-					print $sf->faultcode . "\n";
-					print $sf->faultstring . "\n";
+					echo esc_html( $sf->faultcode . "\n" );
+					echo esc_html( $sf->faultstring . "\n" );
 				}
 
 				if ( '' !== $customer_mob ) {
 					try {
-						$messageIds = $client->send( $customer_mob, $sms_text );
+						$message_id = $client->send( $customer_mob, $sms_text );
 						sleep( 3 );
-						print( $client->deliveryStatus( $messageIds ) );
-						var_dump( $client->accountInfo() );
+						echo esc_html( $client->deliveryStatus( $message_id ) );
+						echo esc_html( $client->accountInfo() );
 					} catch ( SoapFault $sfs ) {
+						// empty
 					}
 				}
-			}
+			}//end if
 
 			?>
 		</div>
