cc776aed6382cb80831ba490959edf27ef97e640
DediData <33499600+Dedi-Data@users.noreply.github.com>
2024-03-28 13:34:20 +0330

    standards

--- includes/forms/formA.php
+++ includes/forms/formA.php
@@ -1,33 +1,31 @@
-﻿
-<form dir="rtl" name="" action="" method="post" class="bank-mellat-form">
-
+﻿<form dir="rtl" name="" action="" method="post" class="bank-mellat-form">
 	<p>نام و نام خانوادگي:<br>
 		<span class="WPBEGPAY">
-			<input type="text" name="bank_mellat_name_family" value="" size="25" >
-		</span> 
+			<input type="text" name="bank_mellat_name_family" value="" size="25">
+		</span>
 	</p>
 	<p>شماره تلفن:<br>
 		<span class="WPBEGPAY">
-			<input type="number" name="bank_mellat_phone" value="" size="25" >
-		</span> 
+			<input type="number" name="bank_mellat_phone" value="" size="25">
+		</span>
 	</p>
 	<p>ايميل:<br>
 		<span class="WPBEGPAY">
-			<input type="email" name="bank_mellat_email" value="" size="25" >
-		</span> 
+			<input type="email" name="bank_mellat_email" value="" size="25">
+		</span>
 	</p>
 	<p>مبلغ(ریال):<br>
 		<span class="WPBEGPAY">
-			<input type="number" name="bank_mellat_price" value="" size="25" >
-		</span> 
+			<input type="number" name="bank_mellat_price" value="" size="25">
+		</span>
 	</p>
 	<p>توضيحات:<br>
 		<span class="WPBEGPAY">
-			<input type="text" name="bank_mellat_description" value="" size="25" >
-		</span> 
+			<input type="text" name="bank_mellat_description" value="" size="25">
+		</span>
 	</p>
-	
-	<p><br>
+	<br />
+	<p>
 		<input type="submit" value="پرداخت" class="WPBEGPAY">
 	</p>
 </form>
